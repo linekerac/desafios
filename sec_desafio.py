202bdf1daab4593ac0ab4a56c89ae9415c047328
linekerac <37567971+linekerac@users.noreply.github.com>
2018-03-24 15:09:11 -0300

    Update sec_desafio.py

--- sec_desafio.py
+++ sec_desafio.py
@@ -2,14 +2,14 @@
 #Imprimir itens da lista 2 que estão contidos na lista 1.
 
 from random import randint
-lista1 = [randint(0, 50)
-for x in range(10)]
+lista1 = [randint(0, 5000000)
+for x in range(500)]
 
-lista2 = [randint(0, 50)
-for x in range(10)]
+lista2 = [randint(0, 5000000)
+for x in range(50000)]
 
 final = list(set(lista1) & set(lista2))
 
-print(lista1)
-print(lista2)
-print(final)
+print("Lista 1: ", lista1)
+print("Lista 2: ", lista2)
+print("Lista Final: ", final)
