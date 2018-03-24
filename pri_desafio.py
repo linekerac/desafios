bbb7519e6d3af1fab8ec7703931f2a6b5ba1c306
linekerac <37567971+linekerac@users.noreply.github.com>
2018-03-24 15:46:46 -0300

    Update pri_desafio.py

--- pri_desafio.py
+++ pri_desafio.py
@@ -2,6 +2,11 @@
 # Lista de 300 números ordenados por ordem crescente.
 # Achar um número aleatório informado pelo usuário da maneira mais eficiente possível.
 
-x = int(input("Digite: "))
-y = list(range(1, 300))
-print(y.index(x)+1)
+x = int(input("Digite um número (1-300): "))
+y = list(range(1, 301))
+try:
+    x == y.index(x) +1
+    print(y.index(x) +1)
+except ValueError:
+    print("Insira um valor válido! (1-300).")
+
